772107d8c6cf30531c4e7392be675775090c723a
JSTheGreat <45407804+JSTheGreat@users.noreply.github.com>
2022-09-17 15:03:16 +0200

    feat: User registration implemented (#4)
    * feat: User registration implemented
    
    * fix: Finding user by id enabled
    
    * fix: Corrections made in service and controller
    
    * fix: Simpler error catching enabled

---
+++ src/main/java/com/dislinkt/post/controller/PersonController.java
@@ -0,0 +1,64 @@
+package com.dislinkt.post.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dislinkt.post.dto.ErrorDTO;
+import com.dislinkt.post.dto.PersonDTO;
+import com.dislinkt.post.dto.ResponseDTO;
+import com.dislinkt.post.service.PersonService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/people")
+public class PersonController {
+    
+    @Autowired
+    private PersonService service;
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErrorDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<ErrorDTO> ret = new ArrayList<ErrorDTO>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            ErrorDTO dto = new ErrorDTO(error.getDefaultMessage());
+            ret.add(dto);
+        });
+        return ret;
+}
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<ResponseDTO<List<PersonDTO>>> getAll(){
+        ResponseDTO<List<PersonDTO>> ret = new ResponseDTO<>(service.findAll());
+        return new ResponseEntity<>(ret, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity addPerson(@RequestHeader("id") UUID id, @Valid @RequestBody PersonDTO dto){
+        try{
+            ResponseDTO<PersonDTO> ret = new ResponseDTO<>(service.create(id, dto));
+            return new ResponseEntity<>(ret, HttpStatus.OK);
+        }
+        catch (Exception ex){
+            ErrorDTO error = new ErrorDTO(ex.getMessage());
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ src/main/java/com/dislinkt/post/service/PersonService.java
@@ -0,0 +1,43 @@
+package com.dislinkt.post.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dislinkt.post.dto.PersonDTO;
+import com.dislinkt.post.mapper.PersonMapper;
+import com.dislinkt.post.model.Person;
+import com.dislinkt.post.repository.PersonRepository;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository repository;
+
+    private PersonMapper mapper = new PersonMapper();
+
+    public List<PersonDTO> findAll(){
+        return mapper.toDtoList(repository.findAll());
+    }
+
+    private Person findOne(UUID id){
+        for (Person person: repository.findAll()){
+            if (person.getId().equals(id))
+                return person;
+        }
+        return null;
+    }
+
+    public PersonDTO create(UUID id, PersonDTO dto) throws Exception{
+        Person person = mapper.toEntity(dto);
+        person.setId(id);
+        if (findOne(person.getId()) != null)
+            throw new Exception("User with given id already exists!");
+        person = repository.save(person);
+        return mapper.toDto(person);
+    }
+    
+}
