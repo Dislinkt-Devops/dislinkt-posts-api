8473fd332c6477eb9669abdcd15fe3346ee40aed
JSTheGreat <45407804+JSTheGreat@users.noreply.github.com>
2022-12-22 20:08:29 +0100

    Dis 24/user messaging check (#10)
    * feat: Initial check for user interaction created
    
    * feat: Initial tests for interaction checks added
    
    * feat: Additional service tests added
    
    * feat: Controller tests added for user interaction

--- src/test/java/com/dislinkt/post/service/PersonServiceTest.java
+++ src/test/java/com/dislinkt/post/service/PersonServiceTest.java
@@ -1,6 +1,7 @@
 package com.dislinkt.post.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.UUID;
@@ -15,7 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dislinkt.post.dto.PersonDTO;
-
+import com.dislinkt.post.model.Person;
+import com.dislinkt.post.repository.PersonRepository;
 import com.dislinkt.post.constants.PersonConstants;
 
 @RunWith(SpringRunner.class)
@@ -25,6 +27,9 @@ public class PersonServiceTest {
 
     @Autowired
     private PersonService service;
+
+    @Autowired
+    private PersonRepository repository;
 
     @Test
     @Transactional
@@ -52,6 +57,75 @@ public class PersonServiceTest {
             service.create(UUID.fromString(PersonConstants.EXISTING_ID), dto);
         } catch (Exception e) {
             assertEquals(e.getMessage(), "User with given id already exists!");
+        }
+    }
+
+    @Test
+    public void testCanInteractWithOK(){
+        try {
+            assertTrue(service.canInteractWith(UUID.fromString(PersonConstants.EXISTING_ID), UUID.fromString(PersonConstants.EXISTING_ID_2)));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void testCanInteractWithNewUser(){
+        PersonDTO dto = new PersonDTO(
+            null, PersonConstants.FIRST_NAME, PersonConstants.LAST_NAME, 
+            PersonConstants.GENDER, PersonConstants.PHONE_NUMBER, null, null, 
+            PersonConstants.PRIVACY);
+
+        try {
+            service.create(UUID.fromString(PersonConstants.NEW_ID), dto);
+            assertFalse(service.canInteractWith(UUID.fromString(PersonConstants.NEW_ID), UUID.fromString(PersonConstants.EXISTING_ID)));
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testCanInteractWithOnesidedFollowing(){
+        try {
+            assertFalse(service.canInteractWith(UUID.fromString(PersonConstants.EXISTING_ID), UUID.fromString(PersonConstants.EXISTING_ID_3)));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCanInteractWithABlocking(){
+        try {
+            assertFalse(service.canInteractWith(UUID.fromString(PersonConstants.EXISTING_ID), UUID.fromString(PersonConstants.EXISTING_ID_4)));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCanInteractWithNonExistingSender(){
+        try {
+            service.canInteractWith(UUID.fromString(PersonConstants.NEW_ID), UUID.fromString(PersonConstants.EXISTING_ID));
+        } catch (Exception e) {
+            assertEquals("sender with given id doesn't exist", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCanInteractWithNonExistingReceiver(){
+        try {
+            service.canInteractWith(UUID.fromString(PersonConstants.EXISTING_ID), UUID.fromString(PersonConstants.NEW_ID));
+        } catch (Exception e) {
+            assertEquals("receiver with given id doesn't exist", e.getMessage());
         }
     }
     
