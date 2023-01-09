6618c546048daafa3afd124397f2af1692283332
JSTheGreat <jovansvorcan14@gmail.com>
2023-01-09 19:33:22 +0100

    feat: Comment repository test added

---
+++ src/test/java/com/dislinkt/post/constants/CommentConstants.java
@@ -0,0 +1,10 @@
+package com.dislinkt.post.constants;
+
+public class CommentConstants {
+
+    public static final String NEW_TEXT = "This is a new comment!";
+
+    public static final int POST_1 = 2;
+    public static final int POST_2 = 3;
+    
+}
