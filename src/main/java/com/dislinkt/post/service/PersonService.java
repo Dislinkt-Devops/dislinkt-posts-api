8371ce47e6e05301d0a9daf57ba4652f5a784216
JSTheGreat <45407804+JSTheGreat@users.noreply.github.com>
2023-01-23 21:49:22 +0100

    Dis 48/user profile (#20)
    * feat: Fetching user profile enabled
    
    * feat: Editing user profile enabled
    
    * fix: Minor error corrected
    
    * chore: Bumped to version 1.1.1

--- src/main/java/com/dislinkt/post/service/PersonService.java
+++ src/main/java/com/dislinkt/post/service/PersonService.java
@@ -3,10 +3,14 @@ package com.dislinkt.post.service;
 import java.util.List;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dislinkt.post.dto.PersonDTO;
+import com.dislinkt.post.enums.Gender;
+import com.dislinkt.post.enums.ProfilePrivacy;
 import com.dislinkt.post.mapper.PersonMapper;
 import com.dislinkt.post.model.Person;
 import com.dislinkt.post.repository.PersonRepository;
@@ -61,6 +65,31 @@ public class PersonService {
             return Boolean.FALSE;
 
         return Boolean.TRUE;
+    }
+
+    public PersonDTO getMyProfile(UUID id) throws Exception {
+        Person user = repository.findById(id).orElse(null);
+        if (user == null)
+            throw new Exception("User with given id doesn't exist!");
+
+        return mapper.toDto(user);
+    }
+
+    public PersonDTO editMyProfile(UUID id, PersonDTO dto) throws Exception {
+        Person user = repository.findById(id).orElse(null);
+        if (user == null)
+            throw new Exception("User with given id doesn't exist!");
+
+        user.setBio(dto.getBio());
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setGender(Gender.valueOf(dto.getGender()));
+        user.setPhoneNumber(dto.getPhoneNumber());
+        user.setPrivacy(ProfilePrivacy.valueOf(dto.getPrivacy()));
+
+        user = repository.save(user);
+
+        return mapper.toDto(user);
     }
     
 }
