5cdd6baccdf4438de90796ec256561e3cec2f4d3
Vladimir Rodusek <86666830+rodusek-v@users.noreply.github.com>
2023-01-25 16:24:14 +0000

    DIS 55 - Feed posts (#28)
    * feat: Add get post for news feed
    
    * fix: Change date persist method

--- src/main/java/com/dislinkt/post/repository/PostRepository.java
+++ src/main/java/com/dislinkt/post/repository/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dislinkt.post.model.Post;
@@ -12,5 +13,16 @@ import com.dislinkt.post.model.Post;
 public interface PostRepository extends JpaRepository<Post, UUID>{
     
     List<Post> findByPersonId(UUID id);
-
+    @Query(
+        value = "SELECT P.* FROM POST P" +
+        " JOIN PERSON PE ON PE.ID = P.PERSON_ID" +
+        " WHERE (PE.PRIVACY = 1 OR PE.ID = ?1 OR EXISTS (" + 
+        " SELECT * FROM FOLLOWERS F WHERE F.FOLLOWER_ID = ?1 AND F.FOLLOWED_ID = PE.ID" +
+        "))" +
+        "AND NOT EXISTS (" + 
+        " SELECT * FROM BLOCKING B WHERE B.BLOCKER_ID IN (?1, PE.ID) AND B.BLOCKED_ID IN (?1, PE.ID)" +
+        ") ORDER BY P.CREATED_AT DESC",
+        nativeQuery = true
+    )
+    List<Post> findAllByViewer(UUID id);
 }
